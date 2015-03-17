78ed5e8b4188400b3a4abdb91eaff20d41c8a7ee
pjuvigny <pierre.juvigny@capgemini.com>
2015-03-17 15:47:45 +0100

    Added mocha tests

---
+++ test/test.js
@@ -0,0 +1,27 @@
+var assert = require("assert")
+
+
+describe('hooks', function () {
+    before(function () {
+        app
+    })
+    after(function () {
+        // runs after all tests in this block
+    })
+    beforeEach(function () {
+        // runs before each test in this block
+    })
+    afterEach(function () {
+        // runs after each test in this block
+    })
+
+    describe('Array', function () {
+        describe('#indexOf()', function () {
+            it('should return -1 when the value is not present', function () {
+                assert.equal(-1, [1, 2, 3].indexOf(5));
+                assert.equal(-1, [1, 2, 3].indexOf(0));
+            })
+        })
+    })
+
+})
